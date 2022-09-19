02406b3f1ce795964b45606c2ed71710aac44a6f
Gustavo Figueiredo <gf1985@gmail.com>
2022-09-19 20:34:39 -0300

    extrai reajuste salário para service

---
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class ReajusteService {
+	
+	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+		BigDecimal salarioAjustado = salarioAtual.add(aumento);
+		funcionario.atualizarSalario(salarioAjustado);
+	}
+
+}
