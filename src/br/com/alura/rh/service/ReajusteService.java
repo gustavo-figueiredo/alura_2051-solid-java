30df79225212a2cbc6e16c3487ac7ba417097565
Gustavo Figueiredo <gf1985@gmail.com>
2022-09-21 14:10:42 -0300

    valida regra data último reajuste

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -2,6 +2,8 @@ package br.com.alura.rh.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
@@ -14,6 +16,14 @@ public class ReajusteService {
 		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
 		}
+		
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		if (mesesDesdeUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalo entre reajustes deve ser de no mínimo 6 meses!");
+		}
+		
 		BigDecimal salarioAjustado = salarioAtual.add(aumento);
 		funcionario.atualizarSalario(salarioAjustado);
 	}
